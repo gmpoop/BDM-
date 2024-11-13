528d83626a0f11e3b5ad50a3d7d77c0b98c612b0
PoliMartell <paola.martellg@uanl.edu.mx>
2024-11-12 19:18:35 -0600

    Cambios en el archivo

--- server/conection.php
+++ server/conection.php
@@ -10,5 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Verificar conexión
 if ($conn->connect_error) {
      die("Conexión fallida: " . $conn->connect_error);
-}
+} 
+
 ?>
